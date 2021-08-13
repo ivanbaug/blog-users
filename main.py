5a1539bcbc3e82a6c4659ca0e38b4d79fc9c1a32
ivanbaug <ivsitioinfo@gmail.com>
2021-08-13 11:12:59 -0500

    uncomment db.create_all

--- main.py
+++ main.py
@@ -30,7 +30,8 @@ Bootstrap(app)
 
 # CONNECT TO DB
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    'DATABASE_URL', 'sqlite:///blog.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -97,7 +98,7 @@ class Comment(db.Model):
 
 
 # Line below only required once, when creating DB.
-# db.create_all()
+db.create_all()
 
 
 # Gravatar implementation
